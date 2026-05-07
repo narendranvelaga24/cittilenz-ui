3df813266f6e40518e3cd669367972fa07069db4
Narendran <175111990+Twenty625@users.noreply.github.com>
2026-05-07 11:08:22 +0530

    npm lint fix

--- src/components/ui/Skeleton.jsx
+++ src/components/ui/Skeleton.jsx
@@ -1,5 +1,10 @@
+import { createElement } from "react";
 import { cn } from "../../lib/utils";
 
 export function Skeleton({ as: Component = "span", className, ...props }) {
-  return <Component aria-hidden="true" className={cn("skeleton", className)} {...props} />;
+  return createElement(Component, {
+    "aria-hidden": "true",
+    className: cn("skeleton", className),
+    ...props,
+  });
 }
